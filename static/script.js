a1814482285918d0ee2f96cedff0f44cd4dd11fb
unknown <sar9206@gmail.com>
2026-01-09 13:27:35 -0500

    acceso a secciones listo

--- static/script.js
+++ static/script.js
@@ -3,9 +3,9 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const cartMenu = document.querySelector('.navbar-right-img')
 const imgMiddle = document.querySelector('.left-image-section')
 
-menuEmail.addEventListener('click',toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu)
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
   aside.classList.add('inactive')
   desktopMenu.classList.toggle('inactive')
 }
@@ -15,17 +15,26 @@ const hamburgerMenu = document.querySelector('.mobile-menu')
 
 hamburger.addEventListener('click', togglehamburgerMenu)
 
-function togglehamburgerMenu(){
+function togglehamburgerMenu() {
   hamburgerMenu.classList.toggle('inactive')
 }
 
 const aside = document.querySelector('.subscription-detail')
 
-function toogleCartMenu(){
+function toogleCartMenu() {
   desktopMenu.classList.add('inactive')
   hamburgerMenu.classList.add('inactive')
   aside.classList.toggle('inactive')
 }
+
+// Close mobile menu when a link is clicked
+const mobileLinks = document.querySelectorAll('.mobile-menu a');
+
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    hamburgerMenu.classList.add('inactive');
+  });
+});
 
 
 
