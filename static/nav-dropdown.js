e4cf0c3f20d189bf9accf1c8bb5418dfe5afc204
unknown <sar9206@gmail.com>
2026-05-02 21:14:46 -0500

    cambio de Version 2026

--- static/nav-dropdown.js
+++
@@ -1,9 +0,0 @@
-// Mobile Dropdown Toggle
-const mobileDropdownToggle = document.querySelector('.mobile-dropdown-toggle span');
-const mobileDropdown = document.querySelector('.mobile-dropdown-toggle');
-
-if (mobileDropdownToggle) {
-    mobileDropdownToggle.addEventListener('click', () => {
-        mobileDropdown.classList.toggle('active');
-    });
-}
